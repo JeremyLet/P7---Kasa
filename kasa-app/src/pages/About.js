0f25d42627c76f87a6741e7adacf7c925a928437
Jérémy <jeremy.letisserand@gmail.com>
2022-12-07 15:07:32 +0100

    <opti>amelioration of about page

--- kasa-app/src/pages/About.js
+++ kasa-app/src/pages/About.js
@@ -1,6 +1,7 @@
 import Section1 from "../components/Section1";
 import Footer from "../components/Footer";
 import Dropdown from "../components/Dropdown";
+import Commercials from "../components/CTA";
 import BannerAboutImage from "../assets/images/BannerAboutImage.png";
 import VectorDOWN from "../assets/images/VectorDOWN.svg";
 
@@ -8,26 +9,16 @@ const About = () => {
 	return (
 		<div>
 			<Section1 image={BannerAboutImage} />
-			<Dropdown
-				title="Fiabilité"
-				details="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-				vector={VectorDOWN}
-			/>
-			<Dropdown
-				title="Respect"
-				details="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-				vector={VectorDOWN}
-			/>
-			<Dropdown
-				title="Service"
-				details="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-				vector={VectorDOWN}
-			/>
-			<Dropdown
-				title="Responsabilité"
-				details="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-				vector={VectorDOWN}
-			/>
+			{[...Array(4)].map((index, nom) => {
+				return (
+					<Dropdown
+						key={nom}
+						title={Commercials[nom].nom}
+						details={Commercials[nom].details}
+						vector={VectorDOWN}
+					/>
+				);
+			})}
 			<Footer />
 		</div>
 	);
