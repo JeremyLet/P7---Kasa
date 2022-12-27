8f8d351ed59f52242d1369b4db78edbde648c2e6
Jérémy <jeremy.letisserand@gmail.com>
2022-12-27 17:14:26 +0100

    <opti>opti page 404 + align styles

--- kasa-app/src/pages/Error404.js
+++ kasa-app/src/pages/Error404.js
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 const Error404 = () => {
 	return (
 		<>
-			<div className="__BlocError404">
-				<h1 className="__BlocError404--Title">404</h1>
-				<h2 className="__BlocError404--Details">
+			<div className="BlocError404">
+				<h1 className="BlocError404--Title">404</h1>
+				<h2 className="BlocError404--Details">
 					Oups, La page que demandez n'existe pas
 				</h2>
-				<Link className="__BlocError404--returnLanding" to="/">
+				<Link className="BlocError404--returnLanding" to="/">
 					Retourner sur la page d'accueil
 				</Link>
 			</div>
